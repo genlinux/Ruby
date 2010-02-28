dfeba338e5f218ff22403969b08c1cd6645ced65
ThillaiArasu <nthillaiarasu@Vaio.(none)>
2010-02-28 18:20:00 +0530

    sample test case but to study a lot

---
+++ Problems/Patterns/empty_test.rb
@@ -0,0 +1,20 @@
+#First pattern test or unit test
+require 'test/unit'
+def empty?(s)
+end
+class EmptyTest < Test::Unit::TestCase
+  def setup
+    @empty_string=''
+    @one_char_string='X'
+    @long_string='The rain in Spain'
+    @empty_array=[]
+    @one_element_array=[1]
+    @long_array=[1,2,3,4,5,6]
+  end
+  def test_empty_on_strings
+    assert empty?(@empty_string)
+    assert ! empty?(@one_char_string)
+    assert ! empty?(@long_string)
+  end
+end
+
