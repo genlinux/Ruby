c5bd1f56fc2eb377c66279acd108aa70be699a9a
genlinux <genlinuxworks@gmail.com>
2010-12-19 21:29:06 +0530

    progrma not works fine with rvm ruby

--- Metaprogramming/delegate.rb
+++ Metaprogramming/delegate.rb
@@ -1,4 +1,30 @@
-#Delegates
 require 'delegate'
+
 class Assistant
+  def initialize(name)
+    @name = name
+  end
+  
+  def read_email
+    "(#{@name}) It's mostly spam."
+  end
+  
+  def check_schedule
+    "(#{@name}) You have a meeting today."
+  end
 end
+
+class Manager < DelegateClass(Assistant)
+  def initialize(assistant)
+    super(assistant)
+  end
+  
+  def attend_meeting
+    "Please hold my calls."
+  end
+end
+puts frank = Assistant.new("Frank")
+anne = Manager.new(frank)
+puts anne.attend_meeting  
+puts anne.read_email       
+puts anne.check_schedule  
